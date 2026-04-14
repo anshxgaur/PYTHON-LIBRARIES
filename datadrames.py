c51ff7378e99aa37e9877f9b2325c62f9544c6a7
ANSH GAUR <anshgaurx@gmail.com>
2026-04-14 15:42:50 +0530

    Update datadrames.py

--- datadrames.py
+++ datadrames.py
@@ -4,3 +4,7 @@ data = { 'marks':[10,20,30,40,50],'gender':['f','m','f','f','m']}
 df = pd.DataFrame(data)
 print(df)
         
+# in order to find the mean of this data:
+df = pd.DataFrame(data)
+mean_val = df['marks'].mean()
+print(mean_val)

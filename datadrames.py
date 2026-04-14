a573c3c5c64042ca54b1932b541f354d1f0328e8
ANSH GAUR <anshgaurx@gmail.com>
2026-04-14 15:38:56 +0530

    Add pandas DataFrame creation with gender and marks

--- datadrames.py
+++ datadrames.py
@@ -1,1 +1,6 @@
 #create a dataframe in which gender and marks will be given and you have to enter atleast 5 entries
+import pandas as pd
+data = { 'marks':[10,20,30,40,50],'gender':['f','m','f','f','m']}
+df = pd.DataFrame(data)
+print(df)
+        
